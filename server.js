48d2561808f30b3eca99c0ca013f9a6dd2387dc5
Rawda Yasser <rawda.developer@gmail.com>
2022-06-12 22:58:25 +0200

    Feat: add prettier and eslint configurations

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static(path.join(__dirname, './static')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './static/index.html'));
 });
+
 app.get('/speakers', (req, res) => {
   res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
