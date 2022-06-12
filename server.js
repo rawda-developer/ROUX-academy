51906bd25ebfbddb9d67e6509f4a50630db0fafe
Rawda Yasser <rawda.developer@gmail.com>
2022-06-13 00:46:08 +0200

    Refactor: add routes in a separate directory

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-
+const routes = require('./routes');
 app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-  res.render('pages/index', {
-    pageTitle: 'Home',
-  });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
