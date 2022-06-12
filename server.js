d9e1847b68afa74647f4092fb75917f1806ab986
Rawda Yasser <rawda.developer@gmail.com>
2022-06-12 22:47:19 +0200

    Refactor: add routes to display static files as well as template files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, './static')));
 app.get('/', (req, res) => {
-  res.send('ROUX Academy');
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
